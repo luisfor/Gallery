cb2186440924093346264f72180131d8c24a690b
luis eduardo canedo torres <luillys83@yahoo.es>
2020-05-15 16:04:43 -0500

    creating user microservice, models

---
+++ Photos/Backend-api/models/user.js
@@ -0,0 +1,17 @@
+'use strict'
+
+//import moongoose
+let mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+
+
+let UserSchema = Schema({
+    name: String,
+    surname: String,
+    email: String,
+    password: String,
+    state: String,
+    date: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('User', UserSchema);

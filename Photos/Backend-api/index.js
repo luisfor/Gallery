d8d40a36604cd8aff3529507dfb71d4c96bb4356
luis eduardo canedo torres <luillys83@yahoo.es>
2020-05-15 12:06:25 -0500

    conection to db mongo

--- Photos/Backend-api/index.js
+++ Photos/Backend-api/index.js
@@ -1,1 +1,9 @@
 'use strict'
+var mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/photos', { useNewUrlParser: true})
+        .then(() => {
+            console.log('Connection Success');
+        })
+        .catch(error => console.log(error));

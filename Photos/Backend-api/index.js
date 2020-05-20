8a945c9f9a5467d62e7dc1242584d6d996f8fc5e
luis eduardo canedo torres <luillys83@yahoo.es>
2020-05-20 17:59:51 -0500

    final configurations and developing the steps for its operation

--- Photos/Backend-api/index.js
+++ Photos/Backend-api/index.js
@@ -2,20 +2,27 @@
 let mongoose = require('mongoose');
 
 let app = require('./app');
+
+//install locale variables
+require('dotenv').config({ path: 'variable.env' });
+
+//local variables and port
+let host = process.env.HOST || '0.0.0.0';
 let port = process.env.PORT || 3000;
+
 
 mongoose.set('useFindAndModify', false);
 mongoose.set('useUnifiedTopology', true);
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/photos', { useNewUrlParser: true})
-        .then(() => {
-            //console.log('Connection Success');
+mongoose.connect(process.env.DB_URL, { useNewUrlParser: true })
+    .then(() => {
+        console.log('Connection Success');
 
-            //Create server
-            app.listen(port, () => {
-                //console.log('Server run Success');
-                
-            })
+        //Create server
+        app.listen(port, host, () => {
+            console.log('Server run Success');
 
         })
-        .catch(error => console.log(error));
+
+    })
+    .catch(error => console.log(error));

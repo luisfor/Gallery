35c76f078c22f01f729d73a126d45baced6aa5b3
luis eduardo canedo torres <luillys83@yahoo.es>
2020-05-21 10:33:19 -0500

    user model in angular

---
+++ Photos/Frontend-ui/src/app/models/user.ts
@@ -0,0 +1,14 @@
+export class User{
+    constructor(
+        public _id: string,
+        public name: string,
+        public surname: string,
+        public email: string,
+        public password: string,
+        public image: string,
+        public role: string,
+        public state: string,
+        public date: string,
+        
+    ){}
+}

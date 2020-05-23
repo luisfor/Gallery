41b993ec56948a81de44dfbf848e3f1a4a12975f
luis eduardo canedo torres <luillys83@yahoo.es>
2020-05-23 01:15:58 -0500

    configuring the panel module in general

---
+++ Photos/Frontend-ui/src/app/panel/panel.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { PanelRoutingModule } from './panel-routing.module';
+
+
+
+//route components
+
+import { MainComponent } from './components/main/main.component';
+import { AddComponent } from './components/add/add.component';
+import { EditComponent } from './components/edit/edit.component';
+import { ListComponent } from './components/list/list.component';
+import { DeleteComponent } from './components/delete/delete.component';
+
+
+@NgModule({
+    declarations: [
+        MainComponent,
+        AddComponent,
+        EditComponent,
+        ListComponent,
+        DeleteComponent
+    ],
+    imports: [
+        CommonModule,
+        FormsModule,
+        HttpClientModule,
+        PanelRoutingModule
+    ],
+    providers: []
+})
+
+export class PanelModule {}

c0e8323793263028a29b426050edfde945c08e58
luis eduardo canedo torres <luillys83@yahoo.es>
2020-05-24 16:26:19 -0500

    creation of the form to register the albums and creation of their methods to register them in the database

--- Photos/Frontend-ui/src/app/panel/components/add-album/add-album.component.ts
+++ Photos/Frontend-ui/src/app/panel/components/add-album/add-album.component.ts
@@ -1,17 +1,66 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Router, ActivatedRoute, Params } from '@angular/router';
+
+import { Album } from '../../../models/album';
+import { UserService } from '../../../services/user.service';
+import { PhotoService } from '../../../services/photo.service';
+import { AlbumService } from '../../../services/album.service';
+import { global } from '../../../services/global';
+
 @Component({
   selector: 'addalbum',
   templateUrl: './add-album.component.html',
-  styleUrls: ['./add-album.component.css']
+  styleUrls: ['./add-album.component.css'],
+  providers: [UserService, PhotoService, AlbumService]
 })
 export class AddAlbumComponent implements OnInit {
   public page_title: string;
-  constructor() {
-    this.page_title = '';
-   }
+  public album: Album;
+  public identity;
+  public token;
+  public status;
+  public url;
+
+  constructor(
+    private _route: ActivatedRoute,
+    private _router: Router,
+    private _userService: UserService,
+    private _photoService: PhotoService,
+    private _albumService: AlbumService
+  ) {
+    this.page_title = 'Add new Album';
+    this.identity = this._userService.getIdentity();
+    this.token = this._userService.getToken();
+    this.album = new Album('', '', this.identity._id, '', '');
+    this.url = global.url;
+  }
+
+
 
   ngOnInit() {
+    //console.log(this._albumService.test());
+    
+  }
+
+  onSubmit(form) {
+   this._albumService.addAlbum(this.token, this.album).subscribe(
+     response => {
+        if (response.album) {
+          this.status = 'success';
+          this.album = response.album;
+          this._router.navigate(['panel/listAlbum']);
+        } else {
+          this.status = 'error';   
+        }
+     },
+     error => {
+       this.status = 'error';
+       console.log(error);
+       
+     }
+   );
+
   }
 
 }
---
+++ Photos/Frontend-ui/src/app/services/album.service.ts
@@ -0,0 +1,72 @@
+
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { global } from './global';
+
+@Injectable()
+export class AlbumService {
+    public url: string;
+    public identity;
+    public token;
+    constructor(private _http: HttpClient) {
+        this.url = global.url;
+    }
+    test() {
+        return "hello service photo";
+    }
+
+    getIdentity() {
+        let identity = JSON.parse(localStorage.getItem('identity'));
+
+        if (identity && identity != null && identity != undefined && identity != "undefined") {
+            this.identity = identity;
+        } else {
+            this.identity = null;
+        }
+        return identity;
+    }
+
+    getToken() {
+        let token = JSON.parse(localStorage.getItem('token'));
+
+        if (token && token != null && token != undefined && token != "undefined") {
+            this.token = token;
+        } else {
+            this.token = null;
+        }
+        return token;
+    }
+
+    addAlbum(token, album): Observable<any> {
+
+        let params = JSON.stringify(album);
+        let headers = new HttpHeaders().set('Content-type', 'application/json')
+            .set('Authorization', this.getToken());
+
+        return this._http.post(this.url + 'album', params, { headers: headers });
+    }
+/*
+    getPhotoByUser(userId): Observable<any> {
+
+        let headers = new HttpHeaders().set('Content-type', 'application/json')
+            .set('Authorization', this.getToken());
+
+        return this._http.get(this.url + 'user-photos/'+userId, { headers: headers });
+
+
+    }
+
+    deletePhotoById(token, id): Observable<any>{
+
+        let headers = new HttpHeaders().set('Content-type', 'application/json')
+        .set('Authorization', this.getToken());
+
+    return this._http.delete(this.url + 'photo/'+id, { headers: headers });
+        
+    }
+    search(searchString): Observable<any>{
+        return this._http.get(this.url+'search/'+searchString);
+    }
+*/
+}
